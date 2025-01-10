1c11914643703ac4f714a2754c51a530c219592f
Raman Kurai <ramankurai27@gmail.com>
2025-01-10 19:27:54 +0530

    Fixing UserAuthentication and landing page done

--- middleware.js
+++ middleware.js
@@ -1,24 +1,25 @@
-import { auth, clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute =  createRouteMatcher([
-    "/dashboard(.*)",
-    "/events(.*)",
-    "/meetings(.*)",
-    "/availability(.*)",
-])
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/events(.*)",
+  "/meetings(.*)",
+  "/availability(.*)",
+]);
 
-export default clerkMiddleware((auth , req)=>
-{
-    if (!auth().userId && isProtectedRoute(req)){
-        return auth().redirectToSignIn();
-    }
+export default clerkMiddleware(async (auth, req) => {
+  const authData = await auth();
+  console.log({authData})
+
+  if (!authData.userId && isProtectedRoute(req)) {
+    return authData.redirectToSignIn();
+  }
 });
-
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
