51e96b3a2563c74ab251ef31e7dcd4f2dea654e7
Raman Kurai <ramankurai27@gmail.com>
2025-01-11 15:58:20 +0530

    update username logic in server action

---
+++ actions/users.js
@@ -0,0 +1,29 @@
+"use server"
+import { db } from "@/lib/prisma";
+import { auth, clerkClient } from "@clerk/nextjs/server"
+
+export async function updateUsername(username) {
+    const {userId} = auth();
+    if(!userId){
+        throw new Error("Unauthorized");
+    }
+
+    const existingUser = await db.user.findUnique({
+        where : {username}
+    })
+
+    if(existingUser == existingUser.id !== userId ){
+        throw new Error("Username is already taken");
+    }
+ // Update username in database
+  await db.user.update({
+    where: { clerkUserId: userId },
+    data: { username },
+  });
+
+  await clerkClient.users.updateUser(userId , {
+    username,
+  })
+
+  return {success : true};
+}
