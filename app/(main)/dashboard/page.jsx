968b8501c4060d612ca1bfd7ebaeb06aa981885d
Raman Kurai <ramankurai27@gmail.com>
2025-01-11 15:34:17 +0530

    update username logic

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { useUser } from "@clerk/nextjs";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usernameSchema } from "@/lib/validator";
+const Dashboardpage = () => {
+  const { user, isLoaded } = useUser();
 
-const page = () => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState : { errors },
+  } = useForm({
+    resolver: zodResolver(usernameSchema),
+  });
+
+  useEffect(() => {
+    setValue("username", user?.username);
+  }, [isLoaded]);
+  const onSubmit = async (data) => {};
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="space-y-8">
+      <Card>
+        <CardHeader>
+          <CardTitle>Welcome , {user?.firstName}</CardTitle>
+          {/* Latest Updates */}
+        </CardHeader>
+      </Card>
 
-export default page
+      <Card>
+        <CardHeader>
+          <CardTitle>Your Unique Link</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <div>
+              <div className="flex items-center gap-2">
+                <span>{window?.location.origin}</span>
+                <Input {...register("username")} placeholder="username" />
+              </div>
+            </div>
+            {errors.username && (
+                <p className="text-red-500 text-sm mt-1">
+                  {errors.username.message}
+                </p>
+              )}
+            <Button>Update UserName</Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Dashboardpage;
