c65288fb891ea506a523c68c9bf499875b0ad02d
Raman Kurai <ramankurai27@gmail.com>
2025-01-14 16:36:04 +0530

    User Availability Page

---
+++ app/(main)/availability/layout.jsx
@@ -0,0 +1,15 @@
+import React, { Suspense } from 'react'
+
+const AvailabilityLayout = ({children}) => {
+  return (
+    <div className='mx-auto' >
+      <Suspense fallback={<div>
+        Loading Availability...
+      </div>}>
+        {children}
+      </Suspense>
+    </div>
+  )
+}
+
+export default AvailabilityLayout
