faa7bd1bdcdb08f354b457bfb7fcaf8bf8dc509d
Raman Kurai <ramankurai27@gmail.com>
2025-01-14 19:04:57 +0530

    getUserAvailability with server actions done

--- app/(main)/availability/page.jsx
+++ app/(main)/availability/page.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from "react";
+import AvailabilityForm from "./_components/availability-form";
+import { getUserAvailability } from "@/actions/availability";
+import { defaultAvailability } from "./data";
 
-const AvailabilityPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+export default async function AvailabilityPage() {
+  const availability = await getUserAvailability();
+
+  return <AvailabilityForm initialData={availability || defaultAvailability} />;
 }
-
-export default AvailabilityPage
