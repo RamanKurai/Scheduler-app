c56e6ed7893b43e221b6e512f7427781287cf957
Raman Kurai <ramankurai27@gmail.com>
2025-01-15 22:10:45 +0530

    add: for custom user page

--- actions/users.js
+++ actions/users.js
@@ -57,34 +57,34 @@ export async function updateUsername(username) {
   }
 }
 
-// export async function getUserByUsername(username) {
-//   const user = await db.user.findUnique({
-//     where: { username },
-//     select: {
-//       id: true,
-//       name: true,
-//       email: true,
-//       imageUrl: true,
-//       events: {
-//         where: {
-//           isPrivate: false,
-//         },
-//         orderBy: {
-//           createdAt: "desc",
-//         },
-//         select: {
-//           id: true,
-//           title: true,
-//           description: true,
-//           duration: true,
-//           isPrivate: true,
-//           _count: {
-//             select: { bookings: true },
-//           },
-//         },
-//       },
-//     },
-//   });
+export async function getUserByUsername(username) {
+  const user = await db.user.findUnique({
+    where: { username },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      imageUrl: true,
+      events: {
+        where: {
+          isPrivate: false,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+        select: {
+          id: true,
+          title: true,
+          description: true,
+          duration: true,
+          isPrivate: true,
+          _count: {
+            select: { bookings: true },
+          },
+        },
+      },
+    },
+  });
 
-//   return user;
-// }
+  return user;
+}
---
+++ app/[username]/[eventId]/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserPage = async ({params}) => {
+    console.log(params.username);
+  return (
+    <div>
+      User Page
+    </div>
+  )
+}
+
+export default UserPage
